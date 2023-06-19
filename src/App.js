d281f78b60a45f9edced85faef30f2be51aa5d5b
Mahmoud Ahmed <Mahmoud.a.dash@gmail.com>
2023-06-19 19:01:36 +0300

    Adding Footer components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import Menu from './components/Menu'
+import Footer from './components/Footer'
 
 function App(){
+    
+
     return (
         <div className='container'>
         <Header/>
         <Menu/>
+        <Footer/>
         </div>
         )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 
 const Footer = () => {
     const hour = new Date().getHours();
-    const operHour = 12;
+    const openHour = 12;
     const closeHour = 22;
-    const isOpen =  (hour >= operHour && hour <= closeHour) ;
+    const isOpen =  (hour >= openHour && hour <= closeHour) ;
+  
 
   return (
-    <footer>
+    <footer className='footer'>
+    {isOpen && <p>open</p> }
+    <div className='order' >
+    <p>
+    We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+    online.
+    </p>
+    <button className='btn' > Order</button>
+    </div>
     </footer>
   )
 }
